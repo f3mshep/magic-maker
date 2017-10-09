efe0591e8ac84ba2c7eb87b0e87f0bf90743da62
Alexandra <github email address>
2017-10-09 15:42:45 -0700

    reversing decklist method

--- app/controllers/DeckController.rb
+++ app/controllers/DeckController.rb
@@ -12,11 +12,12 @@ class DeckController < ApplicationController
 		decklist = Card.create_from_decklist(params[:cards])
 		deck = Deck.create(name: params[:name], description: params[:description])
 		deck.cards << decklist
-		binding.pry
+		redirect "/decks/#{deck.slug}/edit"
 	end
 
 	get '/decks/:name/edit' do
-		
+		deck = Deck.find_by_slug(params[:name])
+		binding.pry
 	end
 
 end
--- app/models/decks.rb
+++ app/models/decks.rb
@@ -1,5 +1,19 @@
 class Deck < ActiveRecord::Base
+
+	include Sluggable::InstanceMethods
+	extend Sluggable::ClassMethods
+
 	has_many :deck_cards
 	belongs_to :user
 	has_many :cards, through: :deck_cards
+
+	def decklist
+		cards = self.cards.collect {|card|card.name}
+		card_count = Hash.new(0)
+		cards.each do |card|
+			card_count[card] += 1
+		end
+		binding.pry
+	end
+
 end
